0a33f555c1585d9ec7f75445985edb46c4416e16
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-05-28 17:38:06 -0300

    chore: adds prettier config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (_r, response) => {
-  response.send('Hello World');
+  response.send('Hello World')
 });
 
 app.listen(3000, () => console.log('server started at http://localhost:3000'));
